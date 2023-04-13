2d61f354bbb7a4a428e0527f7ee71ee84244954e
yuxu <Xx10248432@163.com>
2023-04-13 15:27:06 +0800

    send OK to server,and receive  OPERATIONS from server

--- clientmain.cpp
+++ clientmain.cpp
@@ -8,58 +8,63 @@
 #include <arpa/inet.h>
 #include <string>
 #include <iostream>
+// Included to get the support library
+#include "calcLib.h"
 /* You will to add includes here */
 using namespace std;
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
 #define DEBUG
 
-
-// Included to get the support library
-#include "calcLib.h"
-
-
-void initCalcLib(std::string Desthost, int Destport) {
-
-    // create socket 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    ///定义sockaddr_in
-    struct sockaddr_in servaddr;
-
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(Destport);  //服务器端口
-    servaddr.sin_addr.s_addr = inet_addr(Desthost.c_str());  //服务器ip，inet_addr用于IPv4的IP转换（十进制转换为二进制）
-    //连接服务器，成功返回0，错误返回-1
-    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
-    {
-        perror("connect");
-        exit(1);
-    }
-
-}
-
 int main(int argc, char* argv[]) {
-
-    /*
-      Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port).
-       Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'.
-    */
     char delim[] = ":";
     char* Desthost = strtok(argv[1], delim);
     char* Destport = strtok(NULL, delim);
-    // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-    // *Dstport points to whatever string came after the delimiter. 
-
-    /* Do magic */
     int port = atoi(Destport);
 #ifdef DEBUG
     printf("Host %s, and port %d.\n", Desthost, port);
-    int sockfd;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int recv_num;
+    if (sockfd == -1) {
+        perror("failed to create socket");
+        exit(1);
+    }
+    // 连接服务器
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(Desthost); // 服务器IP地址
+
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("failed to connect server");
+        close(sockfd);
+        exit(1);
+    }
+    char recv_buf[1024];
+    memset(recv_buf, 0, sizeof(recv_buf));
+    // 接收数据
+    if ((recv_num = read(sockfd, recv_buf, sizeof(recv_buf))) == -1) {
+        perror("failed to receive data from server");
+    }
+    else if (recv_num > 0) {
+        printf("%s", recv_buf);
+    }
+    // 发送确认消息
+    const char* confirm_message = "OK\n";
+    send(sockfd, confirm_message, strlen(confirm_message), 0);
+    printf("%s", confirm_message);
+    // 接收新数据
+    memset(recv_buf, 0, sizeof(recv_buf));
+    recv_num = read(sockfd, recv_buf, sizeof(recv_buf));
+    if (recv_num == -1) {
+        perror("failed to receive data from server");
+    }
+    else if (recv_num > 0) {
+        printf("%s", recv_buf);
+    }
 
     close(sockfd);
-
 #endif
-    return 0;
-
 }
